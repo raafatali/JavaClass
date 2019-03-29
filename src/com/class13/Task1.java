66a2f5a9a3f542c5b8d2bd2ecb9c819cf233066d
raafatali <rafattareq90@gmail.com>
2019-03-28 22:10:16 -0400

    Task2

--- src/com/class13/Task1.java
+++ src/com/class13/Task1.java
@@ -1,16 +1,17 @@
 package com.class13;
 
 public class Task1 {
-	
+	//Create a String and print it in reverse order (Sunday → yadnuS).
+
 	
 	public static void main(String [] args) {
 		
 		
-		
-		
-		
-		
+		String name="Sunday";
+        for(int i=name.length()-1;i>=0;i--) {
+            System.out.print(name.charAt(i));
+
 		
 
 
-}}
+}}}
