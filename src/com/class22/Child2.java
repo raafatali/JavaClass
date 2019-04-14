e9cd19fc234ce551b83cd47f9b82e6805f3b7c56
raafatali <rafattareq90@gmail.com>
2019-04-14 15:03:28 -0400

    class 23 code

--- src/com/class22/Child2.java
+++ src/com/class22/Child2.java
@@ -2,16 +2,16 @@ package com.class22;
 
 public class Child2 {
 
-String hairType="curly";
+String hairType="";
 	
 	public static void main(String[] args) {
 		
-		Child1 childObject=new Child1();
-		System.out.println("Child 1 eye color "+childObject.eyeColor);
-		System.out.println("Child 1 hair color "+childObject.hairColor);
-		System.out.println("Child 1 has "+childObject.nose+" nose");
+		Child2 childObject=new Child2();
+		System.out.println("Child 2 eye color "+childObject.eyeColor);
+		System.out.println("Child 2 hair color "+childObject.hairColor);
+		System.out.println("Child 2 has "+childObject.nose+" nose");
 		childObject.sing();
-		childObject.playTennis();
+		childObject.canSwim();
 		
 		Parent parentObject=new Parent();
 		//parentObject.hairType --> not accessible in Parent Class
